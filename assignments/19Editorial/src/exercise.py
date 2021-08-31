d83c85f72acc3bd6a462600446050f0b74a858ac
gloriabecky <a01412781@itesm.mx>
2021-08-31 11:56:50 -0500

    Termine ejercicio 6

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    import math
+    palabras = int(input("Dame el numero de palabras: "))
+    cuartilla = int(math.ceil(palabras / 475))
 
+    precio = (cuartilla * 60)
+    preciototal = precio - (precio * 0.10)
+
+    print("El costo de la publicacion es:", preciototal)
 
 if __name__ == '__main__':
     main()
